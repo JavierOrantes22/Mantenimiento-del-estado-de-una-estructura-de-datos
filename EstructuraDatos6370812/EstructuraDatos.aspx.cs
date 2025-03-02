9c7b4a92e94ad6cfb749d729fcce649a80c60a4a
Javier <xaviorantes457@gmail.com>
2025-03-02 13:28:11 -0600

    Primer Commit

---
+++ EstructuraDatos6370812/EstructuraDatos.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EstructuraDatos6370812
+{
+	public partial class EstructuraDatos : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+          
+            String[] strCategoryArray = new String[5];
+            strCategoryArray[0] = "Postre";
+            strCategoryArray[1] = "Frutas";
+           
+            Session["strCategoryArray"] = strCategoryArray;
+
+            Response.Redirect("CategoryUseArraySessionVariables.aspx");
+
+        }
+    }
+}
